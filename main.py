bf8f60618d452caacddf67299420c4812c81c4c7
JohannesLeppittNTIV <Johannes.Leppitt@elev.ga.ntig.se>
2022-04-29 17:37:32 +0200

    Ukraine and Russia

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ font40 = pygame.font.SysFont('Constantia', 40)
 #define game variables
 rows = 5
 cols = 5
-alien_cooldown = 1000#bullet cooldown in milliseconds
-last_alien_shot = pygame.time.get_ticks()
+russia_cooldown = 1000#bullet cooldown in milliseconds
+last_russia_shot = pygame.time.get_ticks()
 countdown = 3
 last_count = pygame.time.get_ticks()
 game_over = 0#0 is no game over, 1 means player has won, -1 means player has lost
@@ -59,8 +59,8 @@ def draw_text(text, font, text_col, x, y):
 
 
 
-#create spaceship class
-class Spaceship(pygame.sprite.Sprite):
+#create ukraine class
+class Ukraine(pygame.sprite.Sprite):
 	def __init__(self, x, y, health):
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.image.load("assets/mig29.jpg")
@@ -122,14 +122,14 @@ class Bullets(pygame.sprite.Sprite):
 		self.rect.y -= 5
 		if self.rect.bottom < 0:
 			self.kill()
-		if pygame.sprite.spritecollide(self, alien_group, True):
+		if pygame.sprite.spritecollide(self, russia_group, True):
 			self.kill()
 
 
 
 
-#create Aliens class
-class Aliens(pygame.sprite.Sprite):
+#create Russians class
+class Russians(pygame.sprite.Sprite):
 	def __init__(self, x, y):
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.image.load("assets/su34.png")
@@ -147,8 +147,8 @@ class Aliens(pygame.sprite.Sprite):
 
 
 
-#create Alien Bullets class
-class Alien_Bullets(pygame.sprite.Sprite):
+#create Russia Bullets class
+class Russia_Bullets(pygame.sprite.Sprite):
 	def __init__(self, x, y):
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.image.load("assets/russiabullet.png")
@@ -159,10 +159,10 @@ class Alien_Bullets(pygame.sprite.Sprite):
 		self.rect.y += 2
 		if self.rect.top > screen_height:
 			self.kill()
-		if pygame.sprite.spritecollide(self, spaceship_group, False, pygame.sprite.collide_mask):
+		if pygame.sprite.spritecollide(self, ukraine_group, False, pygame.sprite.collide_mask):
 			self.kill()
-			#reduce spaceship health
-			spaceship.health_remaining -= 1
+			#reduce ukraine health
+			ukraine.health_remaining -= 1
 
 
 
@@ -173,25 +173,25 @@ class Alien_Bullets(pygame.sprite.Sprite):
 
 
 #create sprite groups
-spaceship_group = pygame.sprite.Group()
+ukraine_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
-alien_group = pygame.sprite.Group()
-alien_bullet_group = pygame.sprite.Group()
+russia_group = pygame.sprite.Group()
+russia_bullet_group = pygame.sprite.Group()
 
 
-def create_aliens():
-	#generate aliens
+def create_russians():
+	#generate russians
 	for row in range(rows):
 		for item in range(cols):
-			alien = Aliens(100 + item * 100, 100 + row * 70)
-			alien_group.add(alien)
+			russia = Russians(100 + item * 100, 100 + row * 70)
+			russia_group.add(russia)
 
-create_aliens()
+create_russians()
 
 
 #create player
-spaceship = Spaceship(int(screen_width / 2), screen_height - 100, 3)
-spaceship_group.add(spaceship)
+ukraine = Ukraine(int(screen_width / 2), screen_height - 100, 3)
+ukraine_group.add(ukraine)
 
 
 
@@ -205,36 +205,36 @@ while run:
 
 
 	if countdown == 0:
-		#create random alien bullets
+		#create random russia bullets
 		#record current time
 		time_now = pygame.time.get_ticks()
 		#shoot
-		if time_now - last_alien_shot > alien_cooldown and len(alien_bullet_group) < 5 and len(alien_group) > 0:
-			attacking_alien = random.choice(alien_group.sprites())
-			alien_bullet = Alien_Bullets(attacking_alien.rect.centerx, attacking_alien.rect.bottom)
-			alien_bullet_group.add(alien_bullet)
-			last_alien_shot = time_now
+		if time_now - last_russia_shot > russia_cooldown and len(russia_bullet_group) < 5 and len(russia_group) > 0:
+			attacking_russia = random.choice(russia_group.sprites())
+			russia_bullet = Russia_Bullets(attacking_russia.rect.centerx, attacking_russia.rect.bottom)
+			russia_bullet_group.add(russia_bullet)
+			last_russia_shot = time_now
 
-		#check if all the aliens have been killed
-		if len(alien_group) == 0:
+		#check if all the russians have been killed
+		if len(russia_group) == 0:
 			game_over = 1
 
 		if game_over == 0:
-			#update spaceship
-			game_over = spaceship.update()
+			#update ukraine
+			game_over = ukraine.update()
 
 			#update sprite groups
 			bullet_group.update()
-			alien_group.update()
-			alien_bullet_group.update()
+			russia_group.update()
+			russia_bullet_group.update()
 		else:
 			if game_over == -1:
-				draw_text('GAME OVER!', font40, white, int(screen_width / 2 - 100), int(screen_height / 2 + 50))
+				draw_text('UKRAINE DEFEATED!', font40, white, int(screen_width / 2 - 190), int(screen_height / 2 + 50))
 			if game_over == 1:
-				draw_text('YOU WIN!', font40, white, int(screen_width / 2 - 100), int(screen_height / 2 + 50))
+				draw_text('RUSSIA DEFEATED!', font40, white, int(screen_width / 2 - 180), int(screen_height / 2 + 50))
 
 	if countdown > 0:
-		draw_text('GET READY!', font40, white, int(screen_width / 2 - 110), int(screen_height / 2 + 50))
+		draw_text('DEFEND UKRAINE!', font40, white, int(screen_width / 2 - 180), int(screen_height / 2 + 50))
 		draw_text(str(countdown), font40, white, int(screen_width / 2 - 10), int(screen_height / 2 + 100))
 		count_timer = pygame.time.get_ticks()
 		if count_timer - last_count > 1000:
@@ -245,10 +245,10 @@ while run:
 
 
 	#draw sprite groups
-	spaceship_group.draw(screen)
+	ukraine_group.draw(screen)
 	bullet_group.draw(screen)
-	alien_group.draw(screen)
-	alien_bullet_group.draw(screen)
+	russia_group.draw(screen)
+	russia_bullet_group.draw(screen)
 
 
 	#event handlers
